a4ec526ef1e3c364bd69ed16240bf6de16b92ec3
Natasya Natalia <natasyanatalia421@gmail.com>
2026-09-20 16:24:57 +0700

    Update halaman dashboard

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
